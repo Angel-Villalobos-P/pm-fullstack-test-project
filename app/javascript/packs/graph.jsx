3fac15b1ab22f7130794166cd63aae7713f736d6
Angel <angel.villalobos2596@gmail.com>
2024-03-20 04:52:52 -0600

    Postmark Full-Stack Developer Test Project completed

---
+++ app/javascript/packs/graph.jsx
@@ -0,0 +1,135 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+// See react-d3-graph docs at https://danielcaldas.github.io/react-d3-graph/docs/index.html
+import { Graph } from 'react-d3-graph'
+
+const D3_GRAPH_CONFIG = {
+  linkHighlightBehavior: true,
+  width: 900,
+  height: 600,
+  node: {
+    color: 'yellow',
+    size: 120,
+    highlightStrokeColor: 'blue',
+  },
+  link: {
+    highlightColor: '#efefef',
+  },
+}
+
+// custom prop types
+const NodeShape = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+})
+
+const LinkShape = PropTypes.shape({
+  source: PropTypes.string.isRequired,
+  target: PropTypes.string.isRequired,
+})
+
+const SnapshotShape = PropTypes.shape({
+  nodes: PropTypes.arrayOf(NodeShape).isRequired,
+  links: PropTypes.arrayOf(LinkShape).isRequired,
+})
+
+// components
+const Inspector = ({ source, target, topics }) => (
+  <p>
+    {source && target ? (
+      <span>
+        <strong>{source}</strong> and <strong>{target}</strong> chatted about{' '}
+        <em>{topics}</em>
+      </span>
+    ) : (
+      <em>Hover your cursor over a connection line</em>
+    )}
+  </p>
+)
+Inspector.propTypes = {
+  source: PropTypes.string,
+  target: PropTypes.string,
+  topics: PropTypes.string,
+}
+
+const App = ({ snapshot }) => {
+  const [ currentSource, setCurrentSource ] = React.useState()
+  const [ currentTarget, setCurrentTarget ] = React.useState()
+  const [ currentTopics, setCurrentTopics ] = React.useState()
+
+  const handleClickNode = _nodeId => { }
+
+  const handleMouseOverNode = _nodeId => { }
+
+  const handleMouseOutNode = _nodeId => { }
+
+  const handleClickLink = (_source, _target) => { }
+
+  const handleMouseOverLink = (source, target, topics) => {
+    setCurrentSource(source)
+    setCurrentTarget(target)
+    findCurrentTopics(source, target)
+  }
+
+  const handleMouseOutLink = (_source, _target) => {
+    setCurrentSource(undefined)
+    setCurrentTarget(undefined)
+  }
+
+  /**
+   * Finds the current topics based on the source and target parameters.
+   *
+   * @param {string} source - The source parameter.
+   * @param {string} target - The target parameter.
+   */
+  const findCurrentTopics = (source, target) => { 
+    const link = snapshot.links.find(link => link.source === source && link.target === target)
+    if (link && link.topics) {
+      setCurrentTopics(link.topics)
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <Inspector
+          source={currentSource}
+          target={currentTarget}
+          topics={currentTopics}
+        />
+      </div>
+      <Graph
+        id='graph'
+        data={snapshot}
+        config={D3_GRAPH_CONFIG}
+        onClickNode={handleClickNode}
+        onClickLink={handleClickLink}
+        onMouseOverNode={handleMouseOverNode}
+        onMouseOutNode={handleMouseOutNode}
+        onMouseOverLink={handleMouseOverLink}
+        onMouseOutLink={handleMouseOutLink}
+      />
+    </div>
+  )
+}
+App.propTypes = {
+  snapshot: SnapshotShape.isRequired,
+}
+
+const DUMMY_SNAPSHOT = {
+  nodes: [ { id: 'Harry' }, { id: 'Sally' }, { id: 'Alice' } ],
+  links: [
+    { source: 'Harry', target: 'Sally' },
+    { source: 'Harry', target: 'Alice' },
+  ],
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <App snapshot={SNAPSHOT_DATA || DUMMY_SNAPSHOT} />,
+    document
+      .getElementById('layout-wrapper')
+      .appendChild(document.createElement('div'))
+  )
+})
---
+++ app/models/snapshot.rb
@@ -0,0 +1,69 @@
+require 'mail'
+
+class Snapshot < ApplicationRecord
+  serialize :data, coder: JSON
+
+  def self.take
+    connection = Postmark::ApiClient.new(Rails.application.config.x.postmark.api_token)
+
+    messages = connection.get_messages()
+    
+    data = build_graph_structure(messages)
+
+    Snapshot.new(data: data)
+  end
+
+  def self.build_graph_structure(messages)
+    users = Set.new
+    links = []
+    links_index = {}
+
+    messages.each do |message|
+      sender_name = extract_display_name(message[:from])
+      add_users(users, sender_name)
+
+      message[:to].each do |recipient|
+        recipient_name = recipient['Name']
+        add_users(users, recipient_name)
+
+        link_key = [sender_name, recipient_name].sort.join('|')
+        add_topic_to_link(links_index, link_key, message[:subject])
+      end
+    end
+
+    links = build_links_from_index(links_index)
+    
+    { nodes: users, links: links }
+  end
+  
+
+  # Helpers for building graph structure
+
+  def self.extract_display_name(address_string)
+    Mail::Address.new(address_string).display_name
+  end
+
+  def self.extract_email(email_with_name)
+    Mail::Address.new(email_with_name).address
+  end
+
+  def self.add_users(users, recipient_name)
+    users << { id: recipient_name } unless users.any? { |user| user[:id] == recipient_name }
+  end
+
+  def self.add_topic_to_link(links_index, link_key, topic)
+    links_index[link_key] ||= []
+    links_index[link_key] << topic
+  end
+
+  def self.build_links_from_index(links_index)
+    links_index.map do |id, topics|
+      source, target = id.split('|')
+      {
+        source: source,
+        target: target,
+        topics: topics.uniq.join(', ')
+      }
+    end
+  end
+end
